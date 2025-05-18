e91b2de890419dde4478a09c48c6762c0250dd92
DanielTheCodeExplorer <danieladenuga101@gmail.com>
2025-05-18 14:21:14 +0000

    Fix project.yaml format for OpenSAFELY

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -5,10 +5,14 @@ dataset = create_dataset()
 
 index_date = "2020-03-31"
 
+# Getting patients with the index date of 31 of march 2020
 has_registration = practice_registrations.for_patient_on(
     index_date
 ).exists_for_patient()
 
+#Probably changing data type 
 dataset.define_population(has_registration)
 
+#Setting some values in the dataset
 dataset.sex = patients.sex
+dataset.age = patients.age_on(index_date)
